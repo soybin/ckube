0acf1a8cc021e758f3e2e2a13114ff7104ec0e7f
kuwoko <owavemail@gmail.com>
2020-06-29 22:38:25 +0200

    u

--- march.c
+++ march.c
@@ -38,6 +38,10 @@ typedef struct _mat3_3{
 	float3 z;
 } mat3_3;
 
+int int_random_range(int min_range, int max_range) {
+	return (rand() % (max_range - min_range + 1)) + min_range;
+}
+
 float3 float3_add(float3 l, float3 r) {
 	float3 ret = { l.x + r.x, l.y + r.y, l.z + r.z };
 	return ret;
@@ -120,78 +124,32 @@ float3 float3_maxf(float3 l, float r) {
 	return ret;
 }
 
+/* multiply by 3x3 matrix */
+float3 mult_float3_mat3_3(float3 l, mat3_3 r) {
+	return (float3){
+		l.x * r.x.x + l.y * r.y.x + l.z * r.z.x,
+		l.x * r.x.y + l.y * r.y.y + l.z * r.z.y,
+		l.x * r.x.z + l.y * r.y.z + l.z * r.z.z
+	};
+}
+
 /*                                       */
 /*-------- r a y m a r c h i n g --------*/
 /*                                       */
 
-/* globals */
-float GEOMETRY_SIZE = 0.25f;
+float GEOMETRY_SIZE = 1.0f;
 float3 GEOMETRY_REPETITION;
 float3 HALF_GEOMETRY_REPETITION;
-float3 LIGHT_DIRECTION;
 
-enum de_type {
-	SPHERE,
-	CUBE,
-	TORUS
-};
-
-/* distance estimators */
-float de_sphere(float3 point) {
-	return float3_length(point) - GEOMETRY_SIZE;
-}
-
+/* cube distance estimator */
 float de_cube(float3 point) {
 	float3 a = float3_subf(float3_abs(point), GEOMETRY_SIZE);
 	return float3_length(float3_maxf(a, 0.0f)) + MIN(MAX(a.x, MAX(a.y, a.z)), 0.0f);
 }
 
-float de_torus(float3 point) {
-	float x = sqrt(point.x * point.x + point.z * point.z) - GEOMETRY_SIZE;
-	float y = point.y;
-	return sqrt(x * x + y * y) - GEOMETRY_SIZE / 2.0f;
-}
-
+/* theoretical modulo infinity */
 void infinity_operator(float3* point) {
 	*point = float3_sub(float3_mod(float3_add(*point, HALF_GEOMETRY_REPETITION), GEOMETRY_REPETITION), HALF_GEOMETRY_REPETITION);
-}
-
-float3 applyRotation(float3 point, unsigned count) {
-	float ry = count * 1.0f;
-	float rx = count * -.05f;
-	float rz = count * 0.02f;
-
-	/* get rotation */
-	double sinz = sin(rz);
-	double cosz = cos(rz);
-	double siny = sin(ry);
-	double cosy = cos(ry);
-	double sinx = sin(rx);
-	double cosx = cos(rx);
-
-	/* 
-	 * rotation matrix.
-	 * precompute at startup.
-	 * https://en.wikipedia.org/wiki/Rotation_matrix
-	 */
-
-	mat3_3 rm = {
-		(float3){cosz * cosy, cosz * siny * sinx - sinz * cosx, cosz * siny * cosx + sinz * sinx},
-		(float3){sinz * cosy, sinz * siny * sinx + cosy * cosx, sinz * siny * cosx - cosz * sinx},
-		(float3){-siny, cosy * sinx, cosy * cosx}
-	};
-
-	/* apply rotation */
-	point = (float3) {
-		rm.x.x * point.x + rm.y.x * point.y + rm.z.x * point.z,
-		rm.x.y * point.x + rm.y.y * point.y + rm.z.y * point.z,
-		rm.x.z * point.x + rm.y.z * point.y + rm.z.z * point.z
-	};
-
-	return point;
-}
-
-void update_ray_direction(float3** direction_matrix, float y_scaling_factor, unsigned int rows, unsigned int cols, unsigned int fov) {
 }
 
 /*                                       */
@@ -199,26 +157,6 @@ void update_ray_direction(float3** direction_matrix, float y_scaling_factor, uns
 /*                                       */
 
 void print_help() {
-	printf("%s\n", "~usage: march -[fps]");
-	printf("\n");
-	printf("%s\n", "~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	printf("%s\n", "~~~~  g e o m e t r y  ~~~~");
-	printf("%s\n", "~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	printf("%s\n", "-C         -> set shape to a cube");
-	printf("%s\n", "-i [x/y/z] -> repeat shape infinitely along the x, y, or z axis");
-	printf("%s\n", "-c [x/y/z] -> continuously move camera along the x, y, or z axis");
-	printf("%s\n", "~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	printf("%s\n", "~~~~ r e n d e r i n g ~~~~");
-	printf("%s\n", "~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	printf("%s\n", "-M [int]   -> set the maximum step number per ray (default = 16)");
-	printf("%s\n", "-F [int]   ->	set frames per second (default = 20)");
-	printf("%s\n", "-f [int]   ->	set field of view (default = 60)");
-	printf("%s\n", "-m [float] ->	set the minimum distance for a ray to intersect a shape (default = 1e-3)");
-	printf("%s\n", "-v [float] ->	set the vertical stretch factor (default = 1.75)");
-	printf("%s\n", "~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	printf("%s\n", "~~~~   o  t  h  e  r   ~~~~");
-	printf("%s\n", "~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-	printf("%s\n", "-h         -> show this menu");
 }
 
 int main(int argc, char *argv[]) {
@@ -234,18 +172,19 @@ int main(int argc, char *argv[]) {
 	/* renderer */
 	unsigned int fps = 20;
 	unsigned int fov = 45;
-	unsigned int max_step = 32;
-	float min_dist = 1e-3;
+	unsigned int max_step = 16;
+	float min_dist = 1e-2;
 	float y_scaling_factor = 2.0f;
 	/* scene */
-	unsigned int infinity = 0;
-	float geometry_repetition_distance = 1.0f;
-	float3 geometry_repetition = { 0.0f, 0.0f, 0.0f };
-	float3 camera_movement = { 0.0f, 0.0f, 0.0f };
-	float3 camera_offset = { 0.0f, 0.0f, 0.0f };
-	float3 geometry_rotation = { 0.0f, 0.0f, 0.0f };
-
-	enum de_type type = CUBE;
+	unsigned int infinity = 1;
+	float geometry_repetition_distance = 4.0f;
+	float3 geometry_repetition = { 1.0f, 1.0f, 0.0f };
+	float camera_distance = 7.0f;
+	float camera_movement_x = 0.1f;
+	float camera_movement_y = 0.2f;
+	int geometry_rotation_x = int_random_range(1, 10);
+	int geometry_rotation_y = int_random_range(1, 10);
+	int geometry_rotation_z = int_random_range(1, 10);
 
 	/*---- a r g u m e n t s ----*/
 
@@ -255,15 +194,6 @@ int main(int argc, char *argv[]) {
 			return 1;
 		}
 		switch (argv[i][1]) {
-			case 'S':
-				type = SPHERE;
-				break;
-			case 'C':
-				type = CUBE;
-				break;
-			case 'T':
-				type = TORUS;
-				break;
 			case 'i':
 				infinity |= (1 << 0);
 				switch (argv[++i][0]) {
@@ -281,13 +211,10 @@ int main(int argc, char *argv[]) {
 			case 'c':
 				switch (argv[++i][0]) {
 					case 'x':
-						camera_movement.x = 1.0f;
 						break;
 					case 'y':
-						camera_movement.y = 1.0f;
 						break;
 					case 'z':
-						camera_movement.z = 1.0f;
 						break;
 				}
 				break;
@@ -319,25 +246,76 @@ int main(int argc, char *argv[]) {
 	noecho();
 	curs_set(0);
 	timeout(0);
-	
+
 	/* check if terminal supports color */
 	if (!has_colors()) {
 		printf("%s\n", "[-] Your terminal doesn't support colors. Exiting Program");
 		return 1;
 	}
 
-	/* get window width and height */
-	getmaxyx(stdscr, rows, cols);
-
 	/*
 	 * holds the unitary vector direction
 	 * for every pixel in the terminal
 	 */
 	float3* direction_matrix = NULL;
-	
+
+	/*
+	 * holds precomputed rotation matrices
+	 * for every stage of the cube's rotation.
+	 * sin and cos operations are expensive
+	 */
+	int rotation_matrix_array_x_size = 360 / geometry_rotation_x;
+	int rotation_matrix_array_y_size = 360 / geometry_rotation_y;
+	int rotation_matrix_array_z_size = 360 / geometry_rotation_z;
+	mat3_3* rotation_matrix_array_x = (mat3_3*)malloc(rotation_matrix_array_x_size * sizeof(mat3_3));
+	mat3_3* rotation_matrix_array_y = (mat3_3*)malloc(rotation_matrix_array_y_size * sizeof(mat3_3));
+	mat3_3* rotation_matrix_array_z = (mat3_3*)malloc(rotation_matrix_array_z_size * sizeof(mat3_3));	
+	for (int i = 0; i < rotation_matrix_array_x_size; ++i) {
+		/* to radians */
+		float rotation_x = (geometry_rotation_x * i) * M_PI / 180.0f;
+
+		/* get angles */
+		double sin_x = sin(rotation_x);
+		double cos_x = cos(rotation_x);
+
+		*(rotation_matrix_array_x + i) = (mat3_3){
+			(float3){ 1.0f, 0.0f, 0.0f },
+			(float3){ 0.0f, cos_x, -sin_x },
+			(float3){ 0.0f, sin_x, cos_x }
+		};
+	}
+	for (int i = 0; i < rotation_matrix_array_y_size; ++i) {
+		/* to radians */
+		float rotation_y = (geometry_rotation_y * i) * M_PI / 180.0f;
+
+		/* get angles */
+		double sin_y = sin(rotation_y);
+		double cos_y = cos(rotation_y);
+
+		*(rotation_matrix_array_y + i) = (mat3_3){
+			(float3){ cos_y, 0.0f, sin_y },
+			(float3){ 0.0f, 1.0f, 0.0f },
+			(float3){ -sin_y, 0.0f, cos_y }
+		};
+	}
+	for (int i = 0; i < rotation_matrix_array_z_size; ++i) {
+		/* to radians */
+		float rotation_z = (geometry_rotation_z * i) * M_PI / 180.0f;
+
+		/* get angles */
+		double sin_z = sin(rotation_z);
+		double cos_z = cos(rotation_z);
+
+		*(rotation_matrix_array_z + i) = (mat3_3){
+			(float3){ cos_z, -sin_z, 0.0f },
+			(float3){ sin_z, cos_z, 0.0f },
+			(float3){ 0.0f, 0.0f, 1.0f }
+		};
+	}
+
 	/* compute scene after getting fps var */
 	geometry_repetition = float3_multf(geometry_repetition, geometry_repetition_distance);
-	camera_movement = float3_divf(camera_movement, fps);
+	//camera_movement = float3_divf(camera_movement, fps);
 
 	/* update raymarching global vars */
 	GEOMETRY_REPETITION = geometry_repetition;
@@ -355,7 +333,9 @@ int main(int argc, char *argv[]) {
 	/* first frame timestamp */
 	clock_t previous_time = clock();
 
-	for (; run; ++frame_count) {
+	/*---- m a i n    l o o p ----*/
+
+	for (float3 ori = { 0.0f, 0.0f, camera_distance }; run; ++frame_count) {
 
 		/*---- u s e r    i n p u t ----*/
 		
@@ -378,7 +358,7 @@ int main(int argc, char *argv[]) {
 			cols = temp_cols;
 			free(direction_matrix);
 			direction_matrix = (float3*)malloc(rows * cols * sizeof(float3));
-			/* fill up */
+			/* fill up ray directions matrix */
 			for (int r = 0; r < rows; ++r) {
 				for (int c = 0; c < cols; ++c) {
 					float3 dir;
@@ -389,18 +369,13 @@ int main(int argc, char *argv[]) {
 					*(direction_matrix + r * cols + c) = dir;
 				}
 			}
-			update_ray_direction(&direction_matrix, y_scaling_factor, rows, cols, fov);
 		}
 
 		/*---- r e n d e r i n g ----*/
 
-		/* camera position for this frame */
-		float3 ori;
-		if (infinity & 1) {
-			ori = float3_add(float3_add(HALF_GEOMETRY_REPETITION, float3_multf(camera_movement, frame_count)), camera_offset);
-		} else {
-			ori = (float3){ 0.0f, 0.0f, 5.0f * GEOMETRY_SIZE };
-		}
+		/* update ray origin for this frame */
+		ori.x += camera_movement_x;
+		ori.y += camera_movement_y;
 
 		for (int r = 0; r < rows; ++r) {
 			for (int c = 0; c < cols; ++c) {
@@ -411,34 +386,25 @@ int main(int argc, char *argv[]) {
 				int step = 0;
 				for (float total_dist = 0.0f; step < max_step; ++step) {
 					/* compute ray point at this step */
-					point = applyRotation(float3_add(ori, float3_multf(dir, total_dist)), frame_count);
+					point = float3_add(ori, float3_multf(dir, total_dist));
 					/* check if user wants infinity */
 					if (infinity & 1) {
 						infinity_operator(&point);
 					}
-					/* get distance with appropiate DE */
+					/* add rotation */
+					point = mult_float3_mat3_3(point, *(rotation_matrix_array_x + frame_count % rotation_matrix_array_x_size));
+					point = mult_float3_mat3_3(point, *(rotation_matrix_array_y + frame_count % rotation_matrix_array_y_size));
+					point = mult_float3_mat3_3(point, *(rotation_matrix_array_z + frame_count % rotation_matrix_array_z_size));
+					/* get distance */
 					float dist = de_cube(point);
-					/*switch (type) {
-						case SPHERE:
-							dist = de_sphere(point);
-							break;
-						case CUBE:
-							dist = de_cube(point);
-							break;
-						case TORUS:
-							dist = de_torus(point);
-							break;
-					}
 					if (dist < min_dist) {
 						break;
 					}
 					total_dist += dist;
-				}*/
+				}
 				/* in case object was hit, draw */
 				char draw;
 				if (step < max_step) {
-					if (step > 20) draw = '.';
-					else draw = '#' + step - 2;
 					/*---- n o r m a l ----*/
 					const float h = 0.001f;
 					const float3 xyy = { 1.0f, -1.0f, -1.0f };
@@ -481,6 +447,9 @@ int main(int argc, char *argv[]) {
 	/*---- c l e a n u p ----*/
 
 	free(direction_matrix);
+	free(rotation_matrix_array_x);
+	free(rotation_matrix_array_y);
+	free(rotation_matrix_array_z);
 	endwin();
 
 	return 0;
